add1592dfe4192bb65ee633b9b9a8eec725ab4b0
ffmpeg.php <ffmpeg.php@43257b3e-d844-11dd-9042-6d746b85d38b>
2009-01-01 20:58:14 +0000

    initial checkin

---
+++ Makefile
@@ -0,0 +1,15 @@
+EXE=drumroll
+OBJS=usb_utils.o drumroll.o alsamidi.o
+CC=gcc
+CFLAGS=-g
+INCLUDES=/usr/include/SDL
+LIBS=-lusb -lSDL_mixer -lasound
+
+${EXE}: ${OBJS} 
+	${CC} ${CFLAGS} `sdl-config --cflags --libs` ${LIBS} ${OBJS} -o ${EXE}
+
+%.o: %.c
+	${CC} ${CFLAGS} -I${INCLUDES} -c $<
+
+clean:
+	rm ${EXE} ${OBJS}
---
+++ usb_utils.c
@@ -0,0 +1,34 @@
+#include <usb.h>
+
+struct usb_device* get_usb_device(int vendor_id, int device_id)
+{
+    struct usb_bus* bus = NULL;
+    struct usb_device* device = NULL;
+
+    usb_init();
+    usb_find_busses();
+    usb_find_devices();
+
+    for(bus = usb_get_busses(); bus; bus = bus->next) {
+        for(device = bus->devices; device; device = device->next) {
+            if (device->descriptor.idVendor == vendor_id &&
+                    device->descriptor.idProduct == device_id) {
+                return device;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+int claim_device(usb_dev_handle* device_handle, int interface)
+{
+#if LIBUSB_HAS_DETACH_KERNEL_DRIVER_NP
+    // detach kernel hid driver.
+    usb_detach_kernel_driver_np(device_handle, interface);
+#endif
+
+    // claim the device
+    return usb_claim_interface(device_handle, interface);
+}
+
