07e2024a9e48789a2100b71de6f1ae385f796baa
ffmpeg.php <ffmpeg.php@43257b3e-d844-11dd-9042-6d746b85d38b>
2009-01-02 08:22:44 +0000

    opaque alsamidi

--- trunk/alsamidi.h
+++ trunk/alsamidi.h
@@ -17,13 +17,9 @@
  */
 
 #include <stdbool.h>
-#include <alsa/asoundlib.h>
 
-typedef struct {
-    snd_seq_t * handle;
-    int port;
-} AlsaMidi;
-
-bool setup_sequencer(AlsaMidi *seq);
-void send_event(unsigned int note, unsigned int key, int velocity, bool pressed, AlsaMidi *seq);
-void free_sequencer(AlsaMidi* seq);
+typedef struct OpaqueSeq *Seq;
+    
+Seq setup_sequencer();
+void send_event(unsigned int note, unsigned int key, int velocity, bool pressed, Seq);
+void free_sequencer(Seq);
