d9de45439fe4255183952494cafd52d295af8ae0
ffmpeg.php <ffmpeg.php@43257b3e-d844-11dd-9042-6d746b85d38b>
2009-01-07 07:33:38 +0000

    Conditional compilation Check for sound files and error if missing

--- trunk/drumroll.c
+++ trunk/drumroll.c
@@ -14,9 +14,11 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+   */
 
+//#include <config.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
@@ -24,7 +26,6 @@
 #include <getopt.h>
 #include <usb.h>
 
-#include "config.h"
 #include "usb_utils.h"
 
 #ifdef HAVE_LIBASOUND
@@ -55,10 +56,10 @@ int verbose = false;
 
 /* A single drum pad */
 typedef struct {
+#ifdef HAVE_LIBSDL_MIXER
     Sound sound;
+#endif
 } Pad;
-
-
 
 /*
  * Drumkit event loop
@@ -88,22 +89,24 @@ static void start_processing_drum_events(usb_dev_handle* drumkit_handle, Pad *pa
                     send_event(36 + pad_num, 127, true);
                 }
 #endif
-           }
+            }
         }
         last_drum_state = drum_state;
 
+#ifdef HAVE_LIBJACK
         if (jackmidi) {
             set_jack_state(drum_state);
         }
+#endif
 
     }
 
     fprintf(stderr, "ERROR: reading from usb device.\n Reason: %s\n", strerror(errno));
 }
 
-
+#ifdef HAVE_LIBSDL_MIXER
 int load_sounds(Pad *pads) {
-    char filename[256];
+    char filename[1024];
 
     int pad_num;
 
@@ -114,17 +117,23 @@ int load_sounds(Pad *pads) {
             fprintf(stdout, "Loading sound file '%s' into pad %d\n", filename, pad_num + 1);
         }
 
-         pads[pad_num].sound = load_sound(filename, pad_num);
+        if (access(filename, R_OK) < 0) {
+            fprintf(stderr, "ERROR: Can't load sound file '%s'.\n Reason: %s\n", 
+                    filename, strerror(errno));
+            return 1;
+        }
+
+        pads[pad_num].sound = load_sound(filename, pad_num);
 
         if (pads[pad_num].sound == NULL) {
             fprintf(stderr,"Could not load %s\n", filename);
-            return 1;
+            return 2;
         }
     }
 
     return 0;
 }
-
+#endif
 
 void print_usage(char * program_name)
 {
@@ -136,34 +145,36 @@ void print_usage(char * program_name)
 #ifdef HAVE_LIBJACK
     fprintf(stdout, "  -j, --jackmidi\n");
 #endif
+#ifdef HAVE_LIBSDL_MIXER
     fprintf(stdout, "  -n, --nosound\n");
+#endif
     fprintf(stdout, "  -v, --verbose\n");
     fprintf(stdout, "  -V, --version\n");
 }
-
-
-static const struct option long_options[] = {
-    {"help", no_argument,       0, 'h'},
-#ifdef HAVE_LIBASOUND
-    {"alsamidi", no_argument,   0, 'a'},
-    //{"autoconnect-hydrogen", no_argument,   0, 'A'},
-#endif
-#ifdef HAVE_LIBJACK
-    {"jackmidi", no_argument,   0, 'j'},
-#endif
-#ifdef HAVE_LIBSDL_MIXER
-    {"nosound", no_argument,    0, 'n'},
-#endif
-    {"verbose", no_argument,    0, 'v'},
-    {"version", no_argument,    0, 'V'},
-    {0, 0, 0, 0}
-};
-
 
 void parse_options(int argc, char** argv)
 {
     int opt_index = 0;
     char c;
+
+    static const struct option long_options[] = {
+        {"help", no_argument,       0, 'h'},
+#ifdef HAVE_LIBASOUND
+        {"alsamidi", no_argument,   0, 'a'},
+        //{"autoconnect-hydrogen", no_argument,   0, 'A'},
+#endif
+#ifdef HAVE_LIBJACK
+        {"jackmidi", no_argument,   0, 'j'},
+#endif
+#ifdef HAVE_LIBSDL_MIXER
+        {"nosound", no_argument,    0, 'n'},
+#endif
+        {"verbose", no_argument,    0, 'v'},
+        {"version", no_argument,    0, 'V'},
+        {0, 0, 0, 0}
+    };
+
+
     while ((c = getopt_long(argc, argv, "AhjanvV", long_options, &opt_index)) != -1) {
 
         if (c == 0) {
@@ -221,30 +232,33 @@ int main(int argc, char** argv)
     usb_drumkit_device = get_usb_device(USB_VENDOR_ID_DREAM_CHEEKY, USB_DEVICE_ID_ROLL_UP_DRUMKIT);
 
     if (usb_drumkit_device  == NULL) {
-        fprintf(stderr, "ERROR: couldn't find drumkit.\n");
+        fprintf(stderr, "ERROR: couldn't find drumkit. Quitting...\n");
         exit(1);
     }
 
     drumkit_handle = usb_open(usb_drumkit_device);
 
     if (drumkit_handle == NULL) {
-        fprintf(stderr, "ERROR: opening drumkit\n");
+        fprintf(stderr, "ERROR: opening drumkit. Quitting\n");
         exit(2);
     }
 
     if (claim_usb_device(drumkit_handle, 0x00)) {
-        fprintf(stderr, "ERROR: claiming drumkit\n");
+        fprintf(stderr, "ERROR: claiming drumkit. Quitting\n");
         exit(3);
     }
 
 #ifdef HAVE_LIBSDL_MIXER
     if (!nosound) { 
         if (init_audio() != 0) {
-            fprintf(stderr, "ERROR: audio initialization failed\n");
+            fprintf(stderr, "ERROR: audio initialization failed. Quitting\n");
             exit(4);
         }
 
-        load_sounds(pads);
+        if (load_sounds(pads)) {
+            fprintf(stderr, "ERROR: loading sounds. Quitting.\n");
+            exit(5);
+        }
     }
 #endif
 
