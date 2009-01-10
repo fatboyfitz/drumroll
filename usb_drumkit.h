7031cd547670ee9a62136e50f85dfe375cdf859c
ffmpeg.php <ffmpeg.php@43257b3e-d844-11dd-9042-6d746b85d38b>
2009-01-10 06:47:42 +0000

    abstract out drumkit usb specifics

---
+++ usb_drumkit.h
@@ -0,0 +1,10 @@
+#ifndef USB_DRUMKIT_H
+#define USB_DRUMKIT_H
+
+#define USB_DRUMKIT_NUM_PADS 6
+
+int usb_drumkit_open();
+int usb_drumkit_process_events(void (*callback)(int));
+int usb_drumkit_close();
+
+#endif
